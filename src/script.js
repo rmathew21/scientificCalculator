516751c4fb97be5abd1d7dc0950350c81f5f31a9
Rob <47312517+rmathew21@users.noreply.github.com>
2024-06-03 18:48:04 +0000

    added scientific button functionality

--- src/script.js
+++ src/script.js
@@ -10,7 +10,16 @@
                 const convertedValue = currentValue
                 .replace("×", "*")
                 .replace("÷", "/")
-                .replace("%", "*0.01");
+                .replace("%", "*0.01")
+                .replace("sin", "Math.sin")
+                .replace("cos", "Math.cos")
+                .replace("ln", "Math.log")
+                .replace("π", "Math.PI")
+                .replace("log", "Math.log10")
+                .replace("e", "Math.E")
+                .replace("tan", "Math.tan")
+                .replace("√", "Math.sqrt");
+
                 console.log('convertedValue', convertedValue)
                 const result = eval(convertedValue);
                 currentValue = result.toString();
@@ -22,20 +31,28 @@
                 button.addEventListener('click', function() {
                     const value = button.innerText;
 
-                    if (value == "AC") {
-                        currentValue = "";
-                        display.value = currentValue;
-                    } else if (value == '=') {
-                        evaluateResult();
-                    } else {
-                        currentValue += value;
-                        // console.log('currentValue:', currentValue)
-                        display.value = currentValue;
-                    }
+                    try {
+                        if (value === "AC") {
+                            currentValue = "";
+                            display.value = currentValue;
+                        } else if (value === '=') {
+                            evaluateResult();
+                        } else {
+                            currentValue += value;
+                            // console.log('currentValue:', currentValue)
+                            display.value = currentValue;
+                        } }
+                        catch (error) {
+                            console.error(error);
+                            currentValue = "ERROR";
+                            display.value = currentValue;
+                        }
+                    
+                    
 
                     
                     
-                })
+            })
             }
 
             
